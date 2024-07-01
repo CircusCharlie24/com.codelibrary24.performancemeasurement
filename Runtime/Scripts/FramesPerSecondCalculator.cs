36664d02f5b810dd716c45fd9615be2185a8d67f
CircusCharlie24 <circuscharlie2428@gmail.com>
2024-07-01 23:21:28 +0530

    Adding FPS script

---
+++ Runtime/Scripts/FramesPerSecondCalculator.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class FramesPerSecondCalculator : MonoBehaviour
+{
+    [SerializeField] private float updateInterval = 1.0f;
+    [SerializeField] private TextMeshProUGUI fpsText;
+    private float _accum = 0.0f;
+    private int _frames = 0;
+    private float _timeLeft;
+
+    private void Start()
+    {
+        _timeLeft = updateInterval;
+    }
+
+    private void Update()
+    {
+        _timeLeft -= Time.deltaTime;
+        _accum += Time.timeScale / Time.deltaTime;
+        ++_frames;
+
+        if (_timeLeft <= 0.0f)
+        {
+            float fps = _accum / _frames;
+            fpsText.text = "FPS: " + Mathf.RoundToInt(fps);
+
+            _timeLeft = updateInterval;
+            _accum = 0.0f;
+            _frames = 0;
+        }
+    }
+}
+}
